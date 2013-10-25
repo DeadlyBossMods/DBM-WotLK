6c58c6706a4cf07e5fc89a9f2f09e18a6a860bba
Omega <Omega@2e7bb432-e310-41b4-8e50-6cf370805ce3>
2013-10-25 21:54:10 +0000

    Add a SetMinSyncRevision

--- trunk/DBM-Party-WotLK/PitofSaron/ScourgelordTyrannus.lua
+++ trunk/DBM-Party-WotLK/PitofSaron/ScourgelordTyrannus.lua
@@ -3,6 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(36658, 36661)
+mod:SetMinSyncRevision(105)
 mod:SetUsedIcons(8)
 
 mod:RegisterCombat("combat")

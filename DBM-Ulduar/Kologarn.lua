0fb9422bb12f7047d8f5f368ec9f95833f792973
Adam <MysticalOS@users.noreply.github.com>
2015-06-14 08:58:39 +0000

    Detect eyebeams on bigwigs users, and reduce DBM comms at same time.

--- DBM-Ulduar/Kologarn.lua
+++ DBM-Ulduar/Kologarn.lua
@@ -18,6 +18,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_DAMAGE",
 	"SPELL_MISSED",
 	"RAID_BOSS_WHISPER",
+	"CHAT_MSG_ADDON",
 	"UNIT_DIED"
 )
 
@@ -79,19 +80,16 @@ function mod:RAID_BOSS_WHISPER(msg)
 	if msg:find(L.FocusedEyebeam) then
 		specWarnEyebeam:Show()
 		yellBeam:Yell()
-		self:SendSync("EyeBeamOn", UnitGUID("player"))
 	end
 end
 
-function mod:OnSync(msg, guid)
-	local target
-	if guid then
-		target = DBM:GetFullPlayerNameByGUID(guid)
-	end
-	if msg == "EyeBeamOn" and guid then
-		timerNextEyebeam:Start()
-		if target then
-			warnFocusedEyebeam:Show()
+--per usual, use transcriptor message to get messages from both bigwigs and DBM, all without adding comms to this mod at all
+function mod:CHAT_MSG_ADDON(prefix, msg, channel, targetName)
+	if prefix ~= "Transcriptor" then return end
+	if msg:find(L.FocusedEyebeam) then--
+		targetName = Ambiguate(targetName, "none")
+		if self:AntiSpam(5, targetName) then--Antispam sync by target name, since this doesn't use dbms built in onsync handler.
+			warnFocusedEyebeam:Show(targetName)
 			if self.Options.SetIconOnEyebeamTarget then
 				self:SetIcon(target, 5, 8) 
 			end

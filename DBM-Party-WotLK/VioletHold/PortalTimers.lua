fb25349a253785da1e3464be02530bfe5f810d14
Adam <MysticalOS@users.noreply.github.com>
2016-07-26 03:19:57 +0000

    drycode fixes to world state stuff. untested

--- DBM-Party-WotLK/VioletHold/PortalTimers.lua
+++ DBM-Party-WotLK/VioletHold/PortalTimers.lua
@@ -25,7 +25,7 @@ mod:RemoveOption("HealthFrame")
 local lastWave = 0
 
 function mod:UPDATE_WORLD_STATES(args)
-	local text = select(4, GetWorldStateUIInfo(2))
+	local text = select(4, GetWorldStateUIInfo(1))
 	if not text then return end
 	local _, _, wave = string.find(text, L.WavePortal)
 	if not wave then

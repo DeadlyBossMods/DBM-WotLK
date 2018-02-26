c875af669b56025f8cb6463f6ef7ec6efffb16bc
Omega <Omega@2e7bb432-e310-41b4-8e50-6cf370805ce3>
2018-02-26 21:40:12 +0000

    Fix a few wrath bugs

--- trunk/DBM-VoA/Emalon.lua
+++ trunk/DBM-VoA/Emalon.lua
@@ -23,7 +23,7 @@ local specWarnNova			= mod:NewSpecialWarningRun(65279, nil, nil, nil, 4, 2)
 local timerNova				= mod:NewCastTimer(65279, nil, nil, nil, 2)
 local timerNovaCD			= mod:NewCDTimer(45, 65279, nil, nil, nil, 2)--Varies, 45-60seconds in between nova's
 local timerOvercharge		= mod:NewNextTimer(45, 64218, nil, nil, nil, 5, nil, DBM_CORE_DAMAGE_ICON)
-local timerMobOvercharge	= mod:NewTimer(20, "timerMobOvercharge", 64217, nil, nil, 5, nil, DBM_CORE_DAMAGE_ICON)
+local timerMobOvercharge	= mod:NewTimer(20, "timerMobOvercharge", 64217, nil, nil, 5, DBM_CORE_DAMAGE_ICON)
 
 local timerEmalonEnrage		= mod:NewTimer(360, "EmalonEnrage", 26662)
 

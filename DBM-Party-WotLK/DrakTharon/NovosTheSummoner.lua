489314c05e873cac086213fcf9d336f058db8c2d
Adam <MysticalOS@users.noreply.github.com>
2019-02-09 22:19:37 +0000

    Fix up novos to support add count recovery, and improve add messaging and fix first add timer

--- DBM-Party-WotLK/DrakTharon/NovosTheSummoner.lua
+++ DBM-Party-WotLK/DrakTharon/NovosTheSummoner.lua
@@ -14,26 +14,24 @@ mod:RegisterEventsInCombat(
 	"CHAT_MSG_MONSTER_YELL"
 )
 
-local WarnCrystalHandler 	= mod:NewCountAnnounce("ej6378", 2, 59910)
-local warnPhase2			= mod:NewPhaseAnnounce(2)
+local WarnCrystalHandler 		= mod:NewAddsLeftAnnounce("ej6378", 2, 59910)
+local warnPhase2				= mod:NewPhaseAnnounce(2)
 
-local timerCrystalHandler 	= mod:NewNextTimer(15.5, "ej6378", nil, nil, nil, 1, 59910, DBM_CORE_DAMAGE_ICON)
+local timerCrystalHandler 		= mod:NewNextTimer(15.5, "ej6378", nil, nil, nil, 1, 59910, DBM_CORE_DAMAGE_ICON)
 
-local CrystalHandlers = 4
+mod.vb.CrystalHandlers = 4
 
 function mod:OnCombatStart(delay)
-	timerCrystalHandler:Start(25.5-delay)
-	CrystalHandlers = 4
+	timerCrystalHandler:Start(15.9-delay)
+	self.vb.CrystalHandlers = 4
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L.HandlerYell then
-		CrystalHandlers = CrystalHandlers - 1
-		if CrystalHandlers > 0 then
-			WarnCrystalHandler:Show(CrystalHandlers)
+		self.vb.CrystalHandlers = self.vb.CrystalHandlers - 1
+		WarnCrystalHandler:Show(self.vb.CrystalHandlers)
+		if self.vb.CrystalHandlers > 0 then
 			timerCrystalHandler:Start()
-		else
-			WarnCrystalHandler:Show(CrystalHandlers)
 		end
 	elseif msg == L.Phase2 then
 		warnPhase2:Show()

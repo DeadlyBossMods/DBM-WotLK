4c51bd54c9766217601217debd655ed6b3debb8b
MysticalOS <mysticalosx@gmail.com>
2023-02-12 22:16:50 -0500

    fixed bug where animus timer didn't stop when saronite vapor dies

--- DBM-Ulduar/GeneralVezax.lua
+++ DBM-Ulduar/GeneralVezax.lua
@@ -19,6 +19,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 62662",
 	"SPELL_AURA_REMOVED 62662",
 	"SPELL_CAST_SUCCESS 62660 63276 63364",
+	"UNIT_DIED",
 	"RAID_BOSS_EMOTE"
 )
 
@@ -152,6 +153,13 @@ function mod:SPELL_CAST_SUCCESS(args)
 	elseif args.spellId == 63364 then
 		specWarnAnimus:Show()
 		specWarnAnimus:Play("bigmob")
+	end
+end
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 33488 then--Saronite Vapor
+		timerHardmode:Stop()
 	end
 end
 

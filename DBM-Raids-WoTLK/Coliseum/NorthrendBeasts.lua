e4883de5a4e70dd7deebcc6f062aaf9b912688d7
MysticalOS <mysticalosx@gmail.com>
2023-07-08 06:47:43 -0400

    disable broken alert for now

--- DBM-Raids-WoTLK/Coliseum/NorthrendBeasts.lua
+++ DBM-Raids-WoTLK/Coliseum/NorthrendBeasts.lua
@@ -38,7 +38,7 @@ local warnBreath			= mod:NewSpellAnnounce(66689, 2)
 local warnSlimePool			= mod:NewSpellAnnounce(66883, 2, nil, "Melee")
 local warnToxin				= mod:NewTargetAnnounce(66823, 3)
 local warnBile				= mod:NewTargetAnnounce(66869, 3)
-local WarningSnobold		= mod:NewAnnounce("WarningSnobold", 4, 66636, nil, nil, nil, 66636)
+--local WarningSnobold		= mod:NewAnnounce("WarningSnobold", 4, 66636, nil, nil, nil, 66636)
 local warnEnrageWorm		= mod:NewSpellAnnounce(68335, 3)
 local warnCharge			= mod:NewTargetNoFilterAnnounce(52311, 4)
 
@@ -211,7 +211,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args.spellId == 66758 then
 		timerStaggeredDaze:Start()
 	elseif args.spellId == 66636 then
-		WarningSnobold:Show(args.destName)
+--		WarningSnobold:Show(args.destName)
 		timerRisingAnger:Show()
 	elseif args.spellId == 68335 then
 		warnEnrageWorm:Show()
@@ -232,7 +232,7 @@ function mod:SPELL_AURA_APPLIED_DOSE(args)
 		end
 	elseif args.spellId == 66636 then
 		local amount = args.amount or 1
-		WarningSnobold:Show()
+--		WarningSnobold:Show()
 		if amount <= 3 then
 			timerRisingAnger:Show()
 		elseif amount >= 3 then
